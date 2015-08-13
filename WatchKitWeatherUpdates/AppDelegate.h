6113be30a5d3fc0d7e1ed05bf1796d6c10d33a86
Albert Saucedo <saucedoa7@gmail.com>
2015-08-12 21:00:20 -0500

    Initial Commit

---
+++ WatchKitWeatherUpdates/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WatchKitWeatherUpdates
+//
+//  Created by Albert Saucedo on 8/12/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WatchKitWeatherUpdates/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WatchKitWeatherUpdates
+//
+//  Created by Albert Saucedo on 8/12/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
